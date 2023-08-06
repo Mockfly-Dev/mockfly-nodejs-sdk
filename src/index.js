0a31db46454d2e0594bafacbafe6c2e2e7d1cf68
zamarrowski <sergiozamarro@hotmail.com>
2023-08-06 19:01:28 +0200

    identify, evaluation and init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 import axios from 'axios'
 
-const a = () => console.log('ia')
+const host = process.env.MOCKFLY_BACKEND_HOST
 
-export default a
+const Mockfly = function() {
+    this.environment = ''
+    this.authHeader = ''
+    this.evaluationKey = ''
+
+
+    this.init = ({environment, authHeader}) => {
+        this.environment = environment
+        this.authHeader = authHeader
+    }
+
+    this.identify = value => {
+        this.evaluationKey = value
+    }
+
+
+    this.getFlag = async (key) => {
+        if (this.evaluationKey) {
+            const response = await axios.post(`${host}/flags/evaluate`, { keyFlag: key, environment: this.environment, evaluationKey: this.evaluationKey  })
+            return response.data
+        } else {
+            throw new Error('You must identify a user before get a flag. You can use Mockfly.identify(value) function.')
+        }
+
+    }
+
+}
+
+export default Mockfly
